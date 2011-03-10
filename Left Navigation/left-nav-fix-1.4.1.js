4c10c6988c251579b3f87f64ccd2846438bf214e
Kyle Matheny <mathenk2+phaygoweb@gmail.com>
2011-03-10 10:12:38 -0500

    Removed commented-out lines as well as adding credit.

--- Left Navigation/left-nav-fix-1.4.1.js
+++ Left Navigation/left-nav-fix-1.4.1.js
@@ -1,3 +1,8 @@
+/*
+    Kyle A. Matheny
+    Lansing Community College
+*/
+
 var url = window.location.href.toLowerCase(), // Not every browser converts the url to lowercase automatically;
     isEquator = /equator/.test(url);
 
@@ -22,15 +27,3 @@ function cleanLeftNav() {
 if (!isEquator) {
     cleanLeftNav();
 }
-
-/* Compiled: 275 bytes
-    if(!/equator/.test(window.location.href.toLowerCase()))for(var a=document.getElementById("ctl00_ctl00_ctl00_LeftCol_leftNav").getElementsByTagName("a"),b=/[_|>]/,c=a.length,d,e="";c--;){d=a[c];e=d.firstChild.nodeValue;if(b.test(e)||e==e.toLowerCase())d.className="textOnly"};
-*/
-
-/* Simple Compile: 343 bytes
-    var url=window.location.href.toLowerCase(),isEquator=/equator/.test(url);function cleanLeftNav(){for(var c=document.getElementById("ctl00_ctl00_ctl00_LeftCol_leftNav").getElementsByTagName("a"),e=/[_|>]/,d=c.length,b,a="";d--;){b=c[d];a=b.firstChild.nodeValue;if(e.test(a)||a==a.toLowerCase())b.className="textOnly"}}isEquator||cleanLeftNav();
-*/
-
-/* Minified: 497 bytes
-    var url=window.location.href.toLowerCase(),isEquator=/equator/.test(url);function cleanLeftNav(){var objMenuItems=document.getElementById("ctl00_ctl00_ctl00_LeftCol_leftNav").getElementsByTagName("a"),rxBadItem=/[_|>]/,hiddenClass="textOnly",i=objMenuItems.length,tempChild,testString="";while(i--){tempChild=objMenuItems[i],testString=tempChild.firstChild.nodeValue;if(rxBadItem.test(testString)||testString==testString.toLowerCase())tempChild.className=hiddenClass}}if(!isEquator)cleanLeftNav();
-*/
