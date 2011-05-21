3b301dd818b5830e186f5e6a0f3a53678ea58409
Kyle Matheny <mathenk2+phaygoweb@gmail.com>
2011-05-21 11:35:23 -0700

    Actually, version 1.5 --I need to rename the file

--- Left Navigation/left-nav-fix-1.4.1.js
+++ Left Navigation/left-nav-fix-1.4.1.js
@@ -3,27 +3,29 @@
     Lansing Community College
 */
 
-var url = window.location.href.toLowerCase(), // Not every browser converts the url to lowercase automatically;
-    isEquator = /equator/.test(url);
+(/equator/.test(window.location.href.toLowerCase())) ? false : cleanupMenu();
 
-function cleanLeftNav() {
-    var objMenuItems = document.getElementById('ctl00_ctl00_ctl00_LeftCol_leftNav').getElementsByTagName('a'),
-        rxBadItem = /[_|>]/, // Reg Ex with characters to look for
-        hiddenClass = "textOnly", // Class containing display:none
-        i = objMenuItems.length, // Number of menu items
-        tempChild,
-        testString = ''; // Empty variables for While loop
+function cleanupMenu () {
+   var lcc = {
+         leftNav: {
+            menuItems: document.getElementById("ct100_ct100_ct100_LeftCol_leftNav").getElementsByTagName("a"),
+            rxBadItem: /[_|>]/
+         },
+         styling: {
+            hiddenClass: "redhead",
+            showClass: ""
+         }
+      },
+      i = lcc.leftNav.menuItems.length,
+      tempElement = "",
+      tempString = "";
 
-    while (i--) {
-        tempChild = objMenuItems[i];
-        testString = tempChild.firstChild.nodeValue;
-        
-        if (rxBadItem.test(testString) || testString == testString.toLowerCase()) {
-            tempChild.className = hiddenClass;
-        }
-    }
-}
+   while(i--) {
+      tempElement = lcc.leftNav.menuItems[i];
+      tempString = tempElement.firstChild.nodeValue;
 
-if (!isEquator) {
-    cleanLeftNav();
+      if (lcc.leftNav.rxBadItem.test(tempString) || tempString === tempString.toLowerCase()) {
+         tempElement.className = lcc.styling.hiddenClass;
+      }
+   }
 }
