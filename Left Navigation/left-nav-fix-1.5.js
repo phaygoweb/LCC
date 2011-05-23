6177c743c9e80a4d878edb658e605920ad7e46af
Kyle Matheny <mathenk2+phaygoweb@gmail.com>
2011-05-23 08:57:29 -0400

    Rewrite

---
+++ Left Navigation/left-nav-fix-1.5.js
@@ -0,0 +1,26 @@
+var runCleanup = (/equator/.test(window.location.href.toLowerCase())) ? false : cleanupMenu();
+
+function cleanupMenu () {
+   var lcc = {
+         leftNav: {
+            menuItems: document.getElementById("ct100_ct100_ct100_LeftCol_leftNav").getElementsByTagName("a"),
+            rxBadItem: /[_|>]/
+         },
+         classes: {
+            hiddenClass: "redhead",
+            showClass: ""
+         }
+      },
+      i = lcc.leftNav.menuItems.length,
+      tempElement = "",
+      tempString = "";
+   
+   while(i--) {
+      tempElement = lcc.leftNav.menuItems[i];
+      tempString = tempElement.firstChild.nodeValue;
+      
+      if (lcc.leftNav.rxBadItem.test(tempString) || tempString === tempString.toLowerCase()) {
+         tempElement.className = lcc.classes.hiddenClass;
+      }
+   }
+}
